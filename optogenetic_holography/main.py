fixed time averaging, write batch metrics to csv and plot batch plots

optogenetic_holography/main.py
@@ -14,7 +14,7 @@ import torch
 from optogenetic_holography.arg_parser import ArgParser
 from optogenetic_holography.optics import optics_backend as opt
 from optogenetic_holography.utils import init_writer, write_time_average_sequence, config_logger, load_mask, \
-    write_summary
+    write_summary, write_batch_summary_to_csv
 import optogenetic_holography.algorithms as algorithms
 
 
@@ -59,11 +59,6 @@ def main():
     if device.type == 'cuda':
         print(torch.cuda.get_device_properties(device))
 
-    """
-    def vectorised_loss(input, target):
-        return torch.nn.MSELoss(reduction='none')(input, target).mean(dim=(1, 2, 3), keepdim=False)
-    loss_fn = torch.nn.MSELoss() if args.average_batch_grads else vectorised_loss
-    """
     mask = load_mask(args.target_mask_path, device)
     loss_fn = MSE(mask=mask, average_batch_grads=args.average_batch_grads).to(device)
     acc_fn = Accuracy(mask=mask).to(device)
@@ -83,7 +78,7 @@ def main():
     start_time = time.time()
 
     holo_wf, before_bin_metadata = generator(start_wf, target_amp, propagator, writer, param_groups['method_params'])
-
+    assert (holo_wf.phase == start_wf.phase).all() # prevent phase modulation
     end_time = time.time()
 
     holo_wf.plot(summary_dir, param_groups['plot_params'], type='intensity', title='holo', is_holo=True)
@@ -92,8 +87,10 @@ def main():
 
     scale = scale_fn(recon_wf_stack, target_amp, plot_title='recon', init_scale=before_bin_metadata['last_scale'])
     logging.info(f"Optimal scale {scale.squeeze().cpu().numpy():.5f} for recon wf [stack]\n")
-
+    write_batch_summary_to_csv(summary_dir, recon_wf_stack, target_amp, param_groups['method_params'], args.method, scale=scale)
     write_time_average_sequence(writer, recon_wf_stack, target_amp, param_groups['method_params'], scale=scale)
+    if args.plot_ta_batch in ('final', 'both'):
+        recon_wf_stack.plot(summary_dir + '/batch/', param_groups['plot_params'], type='intensity', title='recon_batch', mask=mask, scale=scale)
     recon_wf = recon_wf_stack.time_average()
     #assert scale == scale_fn(recon_wf, target_amp, plot_title='recon') # should be the same
 
@@ -112,9 +109,15 @@ def main():
             before_bin_recon_wf_stack = propagator.forward(before_bin_metadata['holo'])
         else:
             before_bin_recon_wf_stack = before_bin_metadata['recon_wf_stack']
+        scale = before_bin_metadata['last_scale']
+        write_batch_summary_to_csv(summary_dir, before_bin_recon_wf_stack, target_amp, param_groups['method_params'],
+                                   args.method, scale=scale, modulation="before bin")
+        if args.plot_ta_batch in ('before_bin', 'both'):
+            recon_wf_stack.plot(summary_dir + '/batch/', param_groups['plot_params'], type='intensity',
+                                title='before_bin-recon_batch', mask=mask, scale=scale)
+
         before_bin_recon_wf = before_bin_recon_wf_stack.time_average()
 
-        scale = before_bin_metadata['last_scale']
         scale = scale_fn(before_bin_recon_wf, target_amp, plot_title='before_bin_recon', init_scale=scale)  # uncomment if want to recompute scale
         logging.info(f"Optimal scale {scale.squeeze().cpu().numpy():.5f} for before bin recon wf")
         before_bin_recon_wf.plot(summary_dir, param_groups['plot_params'], type='intensity', title='before_bin-recon', mask=mask, scale=scale)

optogenetic_holography/metrics.py
@@ -8,7 +8,7 @@ class MSE(torch.nn.Module):
         self.average_batch_grads = average_batch_grads
         self.mask = mask
 
-    def forward(self, recon_wf, target_amp, scale=None, force_average=False):
+    def forward(self, recon_wf, target_amp, scale=None, force_batch_reduct=''):
         recon_int = (recon_wf.amplitude[recon_wf.roi] ** 2)
         if scale is not None:
             recon_int *= scale
@@ -16,7 +16,7 @@ class MSE(torch.nn.Module):
         if self.mask is not None:
             loss *= self.mask
 
-        if self.average_batch_grads or force_average:
+        if force_batch_reduct == 'avg' or (self.average_batch_grads and force_batch_reduct != 'none'):
             loss = loss.mean(dim=(0, 1, 2, 3), keepdim=False)
         else:
             loss = loss.mean(dim=(1, 2, 3), keepdim=False)
@@ -30,7 +30,7 @@ class Accuracy(torch.nn.Module):
         super(Accuracy, self).__init__()
         self.mask = mask
 
-    def forward(self, recon_wf, target_amp, scale=1):
+    def forward(self, recon_wf, target_amp, scale=1, force_batch_reduct=''):
         if scale is None:
             scale = torch.tensor(1).reshape(recon_wf.batch, 1, 1, 1)
         target_int = torch.square(target_amp[recon_wf.roi])
@@ -38,5 +38,6 @@ class Accuracy(torch.nn.Module):
         if self.mask:
             recon_int *= self.mask
 
-        return torch.sum(recon_int * target_int) / torch.sqrt(torch.sum(target_int ** 2) * torch.sum(recon_int ** 2))
+        dim = (1, 2, 3) if force_batch_reduct == 'none' else (0, 1, 2, 3)
+        return torch.sum(recon_int * target_int, dim) / torch.sqrt(torch.sum(target_int ** 2, dim) * torch.sum(recon_int ** 2, dim))
 

optogenetic_holography/optics/opto/wavefront.py
@@ -1,5 +1,7 @@
 import logging
 import glob
+import os
+
 import cv2
 import torch
 import matplotlib.pyplot as plt
@@ -130,6 +132,8 @@ class Wavefront:
         return torch.allclose(self.u, other_field.u, atol=atol)
 
     def plot(self, dir, options, type='intensity', title='', mask=None, scale=1, is_holo=False, force_colorbar=False):
+        if not os.path.exists(dir):
+            os.makedirs(dir)
         if type == 'intensity':
             img = scale * self.amplitude ** 2
             if options.normalise_plot and not is_holo:
@@ -165,10 +169,11 @@ class Wavefront:
                 plt.close()
 
     def time_average(self, t_start=0, t_end=None):
-        ta_wf = self.copy()
-        ta_wf.batch = 1
+        # time average intensity (not amplitude!) (and discard phase)
+        # fixme numerical precision
+        ta_wf = self.copy(batch=1)
         t_end = self.batch if t_end is None else t_end
-        ta_wf.u = self.u[t_start:t_end, :, :].mean(dim=0, keepdim=True)
+        ta_wf.u = torch.sqrt((self.amplitude[t_start:t_end, :, :] ** 2).mean(dim=0, keepdim=True)).type('torch.ComplexFloatTensor')
         return ta_wf
 
     def copy(self, copy_u=False, batch=None, depth=None, detach=False):

optogenetic_holography/scale_optimiser.py
@@ -27,7 +27,7 @@ class ScaleOptimiser(torch.nn.Module):
         for iter in range(self.iterations):
             optimizer.zero_grad()
             scale = torch.exp(log_scale)
-            loss = self.criterion(recon_wf_copy, target_amp, force_average=True, scale=scale)
+            loss = self.criterion(recon_wf_copy, target_amp, force_batch_reduct='avg', scale=scale)
             loss.backward()
             optimizer.step()
             scheduler.step(loss)

optogenetic_holography/utils.py
@@ -10,8 +10,8 @@ import numpy as np
 import torch
 
 from piq import psnr, ssim
-from torch.nn.functional import mse_loss
 from torch.utils.tensorboard import SummaryWriter
+import pandas as pd
 
 from optogenetic_holography.optics import optics_backend as opt
 
@@ -30,13 +30,10 @@ def config_logger(summary_dir, run_dir):
 
     logging.setLogRecordFactory(record_factory)
 
+
 def mkdir(path):
-    if os.path.exists(path):
-        logging.info(f"Deleting summaries at {path}")
-        for f in os.listdir(path):
-            os.remove(os.path.join(path, f))
-        os.removedirs(path)
-    os.makedirs(path)
+    if not os.path.exists(path):
+        os.makedirs(path)
 
 
 def load_mask(mask_path, device):
@@ -89,7 +86,8 @@ def write_summary(writer, holo, recon_wf, target_amp, iter, context, loss=None, 
     if lr: writer.add_scalar(f'{prefix}/lr', lr, iter)
 
 
-def write_time_average_sequence(writer, recon_wf_stack: opt.Wavefront, target_amp, context, scale):
+def write_time_average_sequence(writer, recon_wf_stack: opt.Wavefront, target_amp, context, scale=1):
+    # time averaged metrics
     prefix = 'Time multiplexing'
     for t in range(0, recon_wf_stack.batch):  # fixme redundant computation
         recon_wf = recon_wf_stack.time_average(t_end=t+1)
@@ -101,3 +99,20 @@ def write_time_average_sequence(writer, recon_wf_stack: opt.Wavefront, target_am
         writer.add_scalar(f'{prefix}/MSE', context.loss_fn(recon_wf, target_amp, scale=scale), t)
         writer.add_scalar(f'{prefix}/SSIM', ssim(recon_wf.normalised_amplitude[recon_wf.roi], target_amp[recon_wf.roi]), t)
         writer.add_scalar(f'{prefix}/PSNR', psnr(recon_wf.normalised_amplitude[recon_wf.roi], target_amp[recon_wf.roi]), t)
+
+
+def write_batch_summary_to_csv(summary_dir, recon_wf_stack, target_amp, context, method_name, scale=1, modulation="final"):
+    # fixme assumes same scale for all batch
+    # sequential metrics
+    loss_bach = opt.Wavefront.to_numpy(context.loss_fn(recon_wf_stack, target_amp, scale=scale, force_batch_reduct='none'))
+    acc_batch = opt.Wavefront.to_numpy(context.acc_fn(recon_wf_stack, target_amp, scale=scale, force_batch_reduct='none'))
+    write_metrics_to_csv(summary_dir, method_name, modulation, acc_batch, loss_bach)
+
+
+def write_metrics_to_csv(dir, method_name, modulation, accuracy, loss):
+    file_path = dir + "/metrics.csv"
+    df = pd.DataFrame({"method": method_name, "mod": modulation, "acc": accuracy, "loss": loss})
+    if not os.path.isfile(file_path):
+        df.to_csv(file_path, index=False)
+    else:
+        df.to_csv(file_path, mode='a', header=False, index=False)